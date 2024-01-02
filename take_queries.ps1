22dd5daa38bf5c6109a1da64acb014cfbfbb52f9
Augusto Gabos <63620556+augustogabos@users.noreply.github.com>
2024-01-02 20:20:43 -0300

    Update take_queries.ps1
    Refactored

--- take_queries.ps1
+++ take_queries.ps1
@@ -1,96 +1,91 @@
 param (
-    [Parameter(Mandatory = $false)] [string] $pat,
-    [Parameter(Mandatory = $false)] [string] $buildsourceversion
+    [Parameter(Mandatory=$false)]
+    [string] $pat,
+    [Parameter(Mandatory=$false)]
+    [string] $buildsourceversion
 )
 
-$folderPath = "/sqlscript/"
-$tablePrefixes = @("scpt_dim") # list of sql script table prefixes here
-$viewPrefix = "scpt_v_"
+function ExecutarSql {
+    param (
+        [string] $Query
+    )
+
+    $clientid = $env:clientid
+    $tenantid = $env:tenantid
+    $secret = $env:secret
+
+    $requestBody = @{
+        resource     = "https://database.windows.net/"
+        grant_type   = "client_credentials"
+        client_id    = $clientid
+        client_secret= $secret
+    }
+
+    $accessTokenResponse = Invoke-RestMethod -Method POST `
+        -Uri "https://login.microsoftonline.com/$tenantid/oauth2/token" `
+        -Body $requestBody `
+        -ContentType "application/x-www-form-urlencoded"
+
+    $access_token = $accessTokenResponse.access_token
+
+    # Execute the SQL query
+    Invoke-Sqlcmd -ServerInstance "syn-prd-url-here" `
+        -Database "db-prd-name-here" `
+        -AccessToken $access_token `
+        -query $Query
+}
+
+$folderPath = "/sqlscript"
+$tablePrefixRegex = "^$folderPath/(scpt_pr_|scpt_ag_|scpt_dm_|scpt_tmp_|scpt_cg_|scpt_ct_)" # Example:  list of sql script table prefixes here
+$viewPrefixRegex = "$folderPath/scpt_v_"
 
 $organization = "org-name"
 $project = "prj-name"
 $repoId = "repo-name"
-$branchName = "main"
 $baseUrl = "https://dev.azure.com/$organization/$project/_apis/git/repositories/$repoId"
 
 $base64Auth = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$pat"))
 $authorizationHeader = @{ Authorization = "Basic $base64Auth" }
 $commitUrl = "$baseUrl/commits/$buildsourceversion/changes?api-version=6.1-preview.1"
-$response = Invoke-RestMethod -Uri $commitUrl -Headers $authorizationHeader -Method Get
 
-if ($response) {
+try {
+    $response = Invoke-RestMethod -Uri $commitUrl -Headers $authorizationHeader -Method Get -ErrorAction Stop
+
     foreach ($change in $response.changes) {
         $filePath = $change.item.path
 
         if ($change.item.gitObjectType -eq "blob" -and $filePath -like "$folderPath*") {
             $fileUrl = "$baseUrl/items?path=$filePath&versionDescriptor.versionType=commit&versionDescriptor.version=$buildsourceversion&recursionLevel=None&api-version=7.0"
-            $fileResponse = Invoke-RestMethod -Uri $fileUrl -Headers $authorizationHeader -Method Get
+            
+            try {
+                $fileResponse = Invoke-RestMethod -Uri $fileUrl -Headers $authorizationHeader -Method Get -ErrorAction Stop
 
-            if ($fileResponse) {
-                $fileName = $fileResponse.name
-                Write-Host "File name: $fileName"
+                if ($fileResponse) {
+                    $fileName = $fileResponse.name
+                    Write-Host "File name: $fileName"
 
-                $query = $fileResponse.properties.content.query
-                Write-Host "Query: $query"
+                    $query = $fileResponse.properties.content.query
+                    Write-Host "Query: $query"
 
-                # Check if the file corresponds to a table or view based on its prefix
-                if ($tablePrefixes | ForEach-Object { $filePath -like "$folderPath$_*" }) {
-                    # Execute the SQL query for tables
-                    $clientid = $env:clientid
-                    $tenantid = $env:tenantid
-                    $secret = $env:secret
+                    # Check if the file corresponds to a table or view based on its prefix
+                    $isTable = $filePath -match $tablePrefixRegex
+                    $isView = $filePath -match $viewPrefixRegex
 
-                    $requestBody = @{
-                        resource = "https://database.windows.net/"
-                        grant_type = "client_credentials"
-                        client_id = $clientid
-                        client_secret = $secret
+                    if ($isTable) {
+                        # Execute the SQL query for tables
+                        ExecutarSql -Query $query
+                    } elseif ($isView) {
+                        # Execute the SQL query for views
+                        ExecutarSql -Query $query
+                    } else {
+                        Write-Host "File does not match any expected prefix."
                     }
-
-                    $accessTokenResponse = Invoke-RestMethod -Method POST `
-                        -Uri "https://login.microsoftonline.com/$tenantid/oauth2/token" `
-                        -Body $requestBody `
-                        -ContentType "application/x-www-form-urlencoded"
-
-                    $access_token = $accessTokenResponse.access_token
-
-                    # Execute the SQL query for tables
-                    Invoke-Sqlcmd -ServerInstance "syn-prd-url-here" `
-                        -Database "db-prd-name-here" `
-                        -AccessToken $access_token `
-                        -query $query
-
-                } elseif ($filePath -like "$folderPath$viewPrefix*") {
-                    # Execute the SQL query for views
-                    $clientid = $env:clientid
-                    $tenantid = $env:tenantid
-                    $secret = $env:secret
-
-                    $requestBody = @{
-                        resource = "https://database.windows.net/"
-                        grant_type = "client_credentials"
-                        client_id = $clientid
-                        client_secret = $secret
-                    }
-
-                    $accessTokenResponse = Invoke-RestMethod -Method POST `
-                        -Uri "https://login.microsoftonline.com/$tenantid/oauth2/token" `
-                        -Body $requestBody `
-                        -ContentType "application/x-www-form-urlencoded"
-
-                    $access_token = $accessTokenResponse.access_token
-
-                    # Execute the SQL query for views
-                    Invoke-Sqlcmd -ServerInstance "syn-prd-url-here" `
-                        -Database "db-prd-name-here" `
-                        -AccessToken $access_token `
-                        -query $query
-                } else {
-                    Write-Host "File does not match any expected prefix."
                 }
+            } catch {
+                Write-Host "Error retrieving file: $_"
             }
         }
     }
-} else {
-    Write-Host "Branch '$branchName' not found in the repository."
+} catch {
+    Write-Host "Error retrieving commit information: $_"
 }
